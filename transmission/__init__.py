59381f5d95f27071176b01e6686fa1df995fa604
Kay <xiaokai_zhou@qq.com>
2022-11-07 15:09:42 +0800

    update modeling

--- transmission/__init__.py
+++ transmission/__init__.py
@@ -1,14 +1,14 @@
-from tenseal import *
-from .tenseal import tenseal_aggregate_server_pb2
-from .tenseal import tenseal_aggregate_server_pb2_grpc
-from .tenseal import tenseal_key_server_pb2
-from .tenseal import tenseal_key_server_pb2_grpc
-from .tenseal import tenseal_parse_server_pb2
-from .tenseal import tenseal_parse_server_pb2_grpc
-from .tenseal import tenseal_data_server_pb2
-from .tenseal import tenseal_data_server_pb2_grpc
-from .tenseal import tenseal_client_proxy_pb2
-from .tenseal import tenseal_client_proxy_pb2_grpc
+# from .tenseal import *
+# from .tenseal import tenseal_aggregate_server_pb2
+# from .tenseal import tenseal_aggregate_server_pb2_grpc
+# from .tenseal import tenseal_key_server_pb2
+# from .tenseal import tenseal_key_server_pb2_grpc
+# from .tenseal import tenseal_parse_server_pb2
+# from .tenseal import tenseal_parse_server_pb2_grpc
+# from .tenseal import tenseal_data_server_pb2
+# from .tenseal import tenseal_data_server_pb2_grpc
+# from transmission.tenseal import tenseal_client_proxy_pb2
+# from .tenseal import tenseal_client_proxy_pb2_grpc
 
 
 
