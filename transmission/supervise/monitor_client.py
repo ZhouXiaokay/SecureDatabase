316669a507d14436853b5122bcafd70a089cee11
qinbozhang <1090587449@qq.com>
2022-12-03 21:23:11 +0800

    fix: update scan ports.

--- transmission/supervise/monitor_client.py
+++ transmission/supervise/monitor_client.py
@@ -2,6 +2,8 @@ import socket
 import threading
 import time
 import datetime
+import multiprocessing
+from multiprocessing import Pool
 
 MAX_BYTES = 1024
 
@@ -47,25 +49,54 @@ def heart_beat_client(host, port, delay):
         IS_SERVER_ALIVE = True
 
 
-def scan_ports(host):
+def scan_ports(host, num_ports):
     """
     :param host: IP.
-    :return: unavailable ports.
+    :return: opened TCP ports.
     """
-    unavailable_ports = []
-    for port in range(1, 10000):
+    opened_ports = []
+    for port in range(num_ports):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(0.5)
             status = s.connect_ex((host, port))
             if status == 0:
-                unavailable_ports.append(port)
+                opened_ports.append(port)
             s.close()
         except Exception as e:
             pass
-    if len(unavailable_ports) != 0:
-        print(unavailable_ports)
+    if len(opened_ports) != 0:
+        print(opened_ports)
     else:
-        print("All ports are available.")
+        print("All ports are closed.")
 
-    return unavailable_ports
+    return opened_ports
+
+def multi_process_scan_ports(host, ports):
+    for port in range(ports, ports + 4096):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        status = s.connect_ex((host, port))
+        if status == 0:
+            print(f"{multiprocessing.current_process().pid}: {port} can be connected by TCP.")
+        s.close()
+
+
+def run_multi_process_scan_ports(host, max_process):
+    """
+    :param host: IP
+    :param max_process: Process pool configuration
+    :return: None
+    """
+    process_pool = Pool(max_process)
+    for index in range(max_process):
+        process_pool.apply_async(multi_process_scan_ports, args=(host, index*4096))
+    process_pool.close()
+    process_pool.join()
+
+    print("All process finished.")
+
+#Debug
+if __name__ == '__main__':
+    run_multi_process_scan_ports('localhost', 10)
+    # scan_ports('localhost', 40960)
